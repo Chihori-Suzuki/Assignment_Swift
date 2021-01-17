80186ff1e5d6a5664cfc940f02e2c571866d69cb
Chihori Suzuki <moonwetwithhoney@mail.com>
2021-01-16 23:45:38 -0800

    Commit

--- Animation_Assignment7/AnimationViewController.swift
+++ Animation_Assignment7/AnimationViewController.swift
@@ -9,10 +9,17 @@ import UIKit
 
 class AnimationViewController: UIViewController {
 
+    var navBarStatus = false
+    let ivOne = UIImageView(image: UIImage(named: "oreos"))
+    let ivTwo = UIImageView(image: UIImage(named: "pizza_pockets"))
+    let ivThree = UIImageView(image: UIImage(named: "pop_tarts"))
+    let ivFour = UIImageView(image: UIImage(named: "popsicle"))
+    let ivFive = UIImageView(image: UIImage(named: "ramen"))
+    
     let navBar: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .gray
+        view.backgroundColor = UIColor(rgb: 0xDDDDDD)
         return view
     }()
     let plusBtn: UIButton = {
@@ -23,6 +30,13 @@ class AnimationViewController: UIViewController {
         btn.addTarget(self, action: #selector(pushBtn(_:)), for: .touchUpInside)
         return btn
     }()
+    let navStackView: UIStackView = {
+        let sv = UIStackView()
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.distribution = .fillEqually
+        sv.axis = .horizontal
+        return sv
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +44,9 @@ class AnimationViewController: UIViewController {
         
         view.addSubview(navBar)
         view.addSubview(plusBtn)
+        
+        navStackView.isHidden = true
+        navBar.addSubview(navStackView)
         
         setView()
         
@@ -42,19 +59,37 @@ class AnimationViewController: UIViewController {
         navBar.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
         
         plusBtn.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -5).isActive = true
-        plusBtn.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -30).isActive = true
+        plusBtn.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -15).isActive = true
         
+        navStackView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        navStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive = true
+        navStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0).isActive = true
+        navStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
         
-        
+        navStackView.addArrangedSubview(ivOne)
+        navStackView.addArrangedSubview(ivTwo)
+        navStackView.addArrangedSubview(ivThree)
+        navStackView.addArrangedSubview(ivFour)
+        navStackView.addArrangedSubview(ivFive)
     }
     
     func animation() {
-        UIView.animate(withDuration: 3.0) {
-            let scaleTransform = CGAffineTransform(scaleX: 1.0, y: 3.5)
-            self.navBar.transform = scaleTransform
-            let translateTransform = CGAffineTransform(translationX: self.view.frame.size.width / 2 - 50, y: self.view.frame.size.height / 2 - 50)
-            let rotateTransform = CGAffineTransform(rotationAngle: .pi/2)
-//            plusBtn.transform = scaleTransform.concatenating(rotateTransform).concatenating(translateTransform)
+        if !navBarStatus {
+            UIView.animate(withDuration: 1.5) {
+                let scaleTransform = CGAffineTransform(scaleX: 1.0, y: 3.5)
+                self.navBar.transform = scaleTransform
+                _ = CGAffineTransform(translationX: self.view.frame.size.width / 2 - 50, y: self.view.frame.size.height / 2 - 50)
+                self.plusBtn.transform = CGAffineTransform(rotationAngle: .pi/4)
+                self.navStackView.isHidden = false
+            }
+            navBarStatus = true
+        } else {
+            UIView.animate(withDuration: 1.5){
+                self.navBar.transform = .identity
+                self.plusBtn.transform = .identity
+                self.navStackView.isHidden = true
+            }
+            navBarStatus = false
         }
     }
     
@@ -74,4 +109,23 @@ class AnimationViewController: UIViewController {
     }
     */
 
+}
+
+
+extension UIColor {
+   convenience init(red: Int, green: Int, blue: Int) {
+       assert(red >= 0 && red <= 255, "Invalid red component")
+       assert(green >= 0 && green <= 255, "Invalid green component")
+       assert(blue >= 0 && blue <= 255, "Invalid blue component")
+
+       self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+   }
+
+   convenience init(rgb: Int) {
+       self.init(
+           red: (rgb >> 16) & 0xDD,
+           green: (rgb >> 8) & 0xDD,
+           blue: rgb & 0xDD
+       )
+   }
 }
